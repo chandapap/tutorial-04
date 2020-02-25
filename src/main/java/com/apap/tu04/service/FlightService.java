cb66184c6aee11322a870c841f6bda372617dd29
Chandra Rahmansyah <chandra.rahmansyah@ui.ac.id>
2020-02-26 00:27:46 +0700

    tutorial-04

---
+++ src/main/java/com/apap/tu04/service/FlightService.java
@@ -0,0 +1,12 @@
+package com.apap.tu04.service;
+
+import java.util.List;
+
+import com.apap.tu04.model.FlightModel;
+import com.apap.tu04.model.PilotModel;
+
+public interface FlightService {
+	void addFlight(FlightModel flight);
+	List<FlightModel> getFlightListByPilot(PilotModel plt);
+	List<FlightModel> getAllFlight();
+}
